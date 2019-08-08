ziqi521/cilium

commit 5aebc46cacba2c164a6bb789f3bc185e5f0256e2

bpf: Attempt pulling skb->data if it is not pulled
If the skb->data_end is too short for Cilium to perform its logic,
attempt to pull it in. If it's still too short, we can legitimately drop
the traffic as "invalid".

Signed-off-by: Joe Stringer <joe@cilium.io>

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=dbf9b1a7ce0d0fe8574d5a7b32cbd374c4f02e0c
+GO_BINDATA_SHA1SUM=9f67fae41b5284a9a894f5a35caa10e48f9957aa
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 

bpf/bpf_overlay.c
@@ -48,7 +48,7 @@ static inline int handle_ipv6(struct __sk_buff *skb, __u32 *identity)
 	bool decrypted;
 
 	/* verifier workaround (dereference of modified ctx ptr) */
-	if (!revalidate_data(skb, &data, &data_end, &ip6))
+	if (!revalidate_data_first(skb, &data, &data_end, &ip6))
 		return DROP_INVALID;
 #ifdef ENABLE_NODEPORT
 	if (!bpf_skip_nodeport(skb)) {
@@ -157,7 +157,7 @@ static inline int handle_ipv4(struct __sk_buff *skb, __u32 *identity)
 	int l4_off;
 
 	/* verifier workaround (dereference of modified ctx ptr) */
-	if (!revalidate_data(skb, &data, &data_end, &ip4))
+	if (!revalidate_data_first(skb, &data, &data_end, &ip4))
 		return DROP_INVALID;
 #ifdef ENABLE_NODEPORT
 	if (!bpf_skip_nodeport(skb)) {

commit d6f98dbc7a88ca223b089e1537c7a36f4f9ed73e

bpf: fix verifier error due to repulling of skb->data/end
After commit 5aebc46cacba ("bpf: Attempt pulling skb->data if it is not pulled")
running the agent with nodeport enabled broke with a verifier error as LLVM
tried to generate code with pkt_end arithmetic which does not work.

  [...]
  level=info msg="Cilium 1.6.90 2c6863714 2019-08-07T09:06:44-07:00 go version go1.11.2 linux/amd64" subsys=daemon
  level=info msg="Envoy version check disabled" subsys=daemon
  level=info msg="clang (7.0.1) and kernel (5.3.0) versions: OK!" subsys=daemon
  [...]
  level=warning msg=" R0=inv4294967154 R1_rw=invP4294967154 R2_rw=invP4294967154 R3=inv1155182100513554433 R4=inv0 R5=inv(id=0) R6=invP0 R7=inv40 R8_rw=invP0 R9=invP0 R10=fp0 fp-56=????mmmm fp-104=??0m0000 fp-112=mmmmmmmm fp-120=mmmmmmmm fp-128=mmmmmmmm fp-136=mmmmmmmm fp-144=00000000 fp-152=mmmmmmmm fp-160=mmmmmmmm fp-168=????0000 fp-176=00000000 fp-184=00000000 fp-192=00000000 fp-200_r=ctx" subsys=daemon
  level=warning msg="parent didn't have regs=100 stack=0 marks" subsys=daemon
  level=warning msg="last_idx 306 first_idx 297" subsys=daemon
  level=warning msg="regs=100 stack=0 before 306: (18) r2 = 0xffffff72" subsys=daemon
  level=warning msg="regs=100 stack=0 before 305: (b7) r8 = 0" subsys=daemon
  level=warning msg="907: (61) r1 = *(u32 *)(r7 +80)" subsys=daemon
  level=warning msg="908: (61) r9 = *(u32 *)(r7 +76)" subsys=daemon
  level=warning msg="909: (18) r8 = 0xffffff7a" subsys=daemon
  level=warning msg="911: (b7) r6 = 0" subsys=daemon
  level=warning msg="912: (67) r1 <<= 32" subsys=daemon
  level=warning msg="R1 pointer arithmetic on pkt_end prohibited" subsys=daemon
  level=warning msg="processed 191 insns (limit 1000000) max_states_per_insn 0 total_states 8 peak_states 8 mark_read 5" subsys=daemon
  level=warning subsys=daemon
  level=warning msg="Error filling program arrays!" subsys=daemon
  level=warning msg="Unable to load program" subsys=daemon
  [...]

Fix to force intermediate writes by using WRITE_ONCE() to avoid such code generation.

Fixes: 5aebc46cacba ("bpf: Attempt pulling skb->data if it is not pulled")

bpf/lib/common.h
@@ -126,8 +126,10 @@ static inline bool __revalidate_data(struct __sk_buff *skb, void **data_,
 	if (data + ETH_HLEN + l3_len > data_end)
 		return false;
 
-	*data_ = data;
-	*data_end_ = data_end;
+	/* Verifier workaround: pointer arithmetic on pkt_end prohibited. */
+	WRITE_ONCE(*data_, data);
+	WRITE_ONCE(*data_end_, data_end);
+
 	*l3 = data + ETH_HLEN;
 	return true;
 }

daemon/bpf.sha
@@ -1,2 +1,2 @@
-GO_BINDATA_SHA1SUM=9f67fae41b5284a9a894f5a35caa10e48f9957aa
+GO_BINDATA_SHA1SUM=c1d1facfa6bcbe181901e3b43a30b9150b5a7efd
 BPF_FILES=../bpf/COPYING ../bpf/Makefile ../bpf/Makefile.bpf ../bpf/bpf_alignchecker.c ../bpf/bpf_features.h ../bpf/bpf_hostdev_ingress.c ../bpf/bpf_ipsec.c ../bpf/bpf_lb.c ../bpf/bpf_lxc.c ../bpf/bpf_netdev.c ../bpf/bpf_network.c ../bpf/bpf_overlay.c ../bpf/bpf_sock.c ../bpf/bpf_xdp.c ../bpf/cilium-map-migrate.c ../bpf/filter_config.h ../bpf/include/bpf/api.h ../bpf/include/elf/elf.h ../bpf/include/elf/gelf.h ../bpf/include/elf/libelf.h ../bpf/include/iproute2/bpf_elf.h ../bpf/include/linux/bpf.h ../bpf/include/linux/bpf_common.h ../bpf/include/linux/byteorder.h ../bpf/include/linux/byteorder/big_endian.h ../bpf/include/linux/byteorder/little_endian.h ../bpf/include/linux/icmp.h ../bpf/include/linux/icmpv6.h ../bpf/include/linux/if_arp.h ../bpf/include/linux/if_ether.h ../bpf/include/linux/if_packet.h ../bpf/include/linux/in.h ../bpf/include/linux/in6.h ../bpf/include/linux/ioctl.h ../bpf/include/linux/ip.h ../bpf/include/linux/ipv6.h ../bpf/include/linux/perf_event.h ../bpf/include/linux/swab.h ../bpf/include/linux/tcp.h ../bpf/include/linux/type_mapper.h ../bpf/include/linux/udp.h ../bpf/init.sh ../bpf/lib/arp.h ../bpf/lib/common.h ../bpf/lib/config.h ../bpf/lib/conntrack.h ../bpf/lib/conntrack_map.h ../bpf/lib/csum.h ../bpf/lib/dbg.h ../bpf/lib/drop.h ../bpf/lib/encap.h ../bpf/lib/eps.h ../bpf/lib/eth.h ../bpf/lib/events.h ../bpf/lib/icmp6.h ../bpf/lib/ipv4.h ../bpf/lib/ipv6.h ../bpf/lib/l3.h ../bpf/lib/l4.h ../bpf/lib/lb.h ../bpf/lib/lxc.h ../bpf/lib/maps.h ../bpf/lib/metrics.h ../bpf/lib/nat.h ../bpf/lib/nat46.h ../bpf/lib/nodeport.h ../bpf/lib/policy.h ../bpf/lib/tailcall.h ../bpf/lib/trace.h ../bpf/lib/utils.h ../bpf/lib/xdp.h ../bpf/lxc_config.h ../bpf/netdev_config.h ../bpf/node_config.h ../bpf/probes/raw_change_tail.t ../bpf/probes/raw_fib_lookup.t ../bpf/probes/raw_insn.h ../bpf/probes/raw_invalidate_hash.t ../bpf/probes/raw_lpm_map.t ../bpf/probes/raw_lru_map.t ../bpf/probes/raw_main.c ../bpf/probes/raw_map_val_adj.t ../bpf/probes/raw_mark_map_val.t ../bpf/probes/raw_sock_cookie.t ../bpf/run_probes.sh ../bpf/sockops/Makefile ../bpf/sockops/bpf_redir.c ../bpf/sockops/bpf_sockops.c ../bpf/sockops/bpf_sockops.h ../bpf/sockops/sockops_config.h 