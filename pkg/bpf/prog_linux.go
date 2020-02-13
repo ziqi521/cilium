fea599cec72802989a3bed7a37098fac153efb52
Daniel Borkmann <daniel@iogearbox.net>
2020-02-13 15:07:58 +0100

    keep attr alive as well
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- pkg/bpf/prog_linux.go
+++ pkg/bpf/prog_linux.go
@@ -127,6 +127,7 @@ func GetProgInfoByFD(fd int) (ProgInfo, error) {
 	ret, _, err := unix.Syscall(unix.SYS_BPF, BPF_OBJ_GET_INFO_BY_FD, uintptr(unsafe.Pointer(&attr)), unsafe.Sizeof(attr))
 	runtime.KeepAlive(info)
 	runtime.KeepAlive(attrInfo)
+	runtime.KeepAlive(attr)
 	if option.Config.MetricsConfig.BPFSyscallDurationEnabled {
 		metrics.BPFSyscallDuration.WithLabelValues(metricOpObjGetInfoByFD, metrics.Errno2Outcome(err)).Observe(duration.End(err == 0).Total().Seconds())
 	}
