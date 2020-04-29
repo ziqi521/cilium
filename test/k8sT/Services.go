5dad613227a25d25f81992e2567cadfb1e3bf4ba
Quentin Monnet <quentin@isovalent.com>
2020-04-29 17:10:16 +0100

    TEST - Do not merge
    Testing PR for testing tests. In particular, trying to reproduce # 11094
    
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- test/k8sT/Services.go
+++ test/k8sT/Services.go
@@ -967,6 +967,7 @@ var _ = Describe("K8sServicesTest", func() {
 		}
 
 		SkipItIf(helpers.RunsWithoutKubeProxy, "Tests NodePort (kube-proxy)", func() {
+			Expect("FOO").Should(BeNil(), "testing stuff")
 			testNodePort(false)
 		})
 
